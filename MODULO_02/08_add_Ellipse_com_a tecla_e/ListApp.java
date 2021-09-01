0d2f5daf5cd04dee6a161af33f6e26449575b993
Oseas Andre <oseasx65@gmail.com>
2021-09-01 20:02:44 -0300

    adicionar Ellipse com a tecla e, OK

---
+++ MODULO_02/08_add_Ellipse_com_a tecla_e/ListApp.java
@@ -0,0 +1,68 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Random;
+
+import figures.*;
+
+class ListApp
+{
+	public static void main(String[] args)
+	{
+		ListFrame frame = new ListFrame();
+		frame.setVisible(true);
+	}	
+}
+
+class ListFrame extends JFrame
+{
+	ArrayList<Rect> rs = new ArrayList<Rect>();
+	ArrayList<Ellipse> es = new ArrayList<Ellipse>();
+	Random rand = new Random();
+	
+	ListFrame() 
+	{
+		this.addWindowListener (
+		    new WindowAdapter() 
+		    {
+		        public void windowClosing (WindowEvent e) {
+		            System.exit(0);
+		        }
+		    }
+		);
+		
+		this.addKeyListener(
+			new KeyAdapter() {
+				public void keyPressed(KeyEvent evt){
+				int x = rand.nextInt(350);
+				int y = rand.nextInt(350);
+				int w = rand.nextInt(50);
+				int h = rand.nextInt(50);
+				if( evt.getKeyChar() == 'r'){ 
+					rs.add( new Rect(x, y, w, h, Color.BLACK, Color.WHITE) );
+					repaint();
+				}else if( evt.getKeyChar() == 'e'){
+					es.add( new Ellipse(x, y, w, h, Color.BLACK, Color.WHITE) );
+					repaint();
+				}
+			}
+		}
+			
+		);
+		this.getContentPane().setBackground(Color.WHITE);
+		this.setTitle("Lista de Retangulos");
+        	this.setSize(350, 350);
+	}
+
+	public void paint(Graphics g)
+	{
+		super.paint(g);
+		for( Rect r: this.rs){
+			r.paint(g);
+		}
+		for( Ellipse e: this.es){
+			e.paint(g);
+		}
+	}
+}
